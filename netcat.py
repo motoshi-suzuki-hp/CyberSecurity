a0a6510e35c41b642480122d131f009d6a55590e
Motoshi Suzuki <101000230+motoshi-suzuki-hp@users.noreply.github.com>
2024-03-16 18:53:23 +0900

    netcat written

--- netcat.py
+++ netcat.py
@@ -8,7 +8,7 @@ import textwrap
 import threading
 import socket
 
-def execure(cmd):
+def execute(cmd):
     cmd = cmd.split()
 
     if not cmd:
@@ -27,6 +27,96 @@ def execure(cmd):
         return output.decode()
     
 
+class NetCat:
+    def __init__(self, args, buffer=None):
+        self.args = args
+        self.buffer = buffer
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def run(self):
+        if self.args.listen:
+            self.listen()
+        else:
+            self.send()
+
+    def send(self):
+        self.socket.connect((self.args.target, self.args.port))
+        if self.buffer:
+            self.socket.send(self.buffer)
+
+        try:
+            while True:
+                recv_len = 1
+                response =  ''
+                while recv_len:
+                    data = self.socket.recv(4096)
+                    recv_len = len(data)
+                    response += data.decode()
+                    if recv_len < 4096:
+                        break
+
+                if response:
+                    print(response)
+                    buffer = input('>')
+                    buffer += '\n'
+                    self.socket.send(buffer.encode())
+        except KeyboardInterrupt:
+            print('User terminated.')
+            self.socket.close()
+            sys.exit()
+        except EOFError as e:
+            print(e)
+
+
+    def listen(self):
+        self.socket.bind((self.args.target, self.args.port))
+        self.socket.listen(5)
+        while True:
+            client_socket, _ = self.socket.accept()
+            client_thread = threading.Thread(
+                target=self.handle, args=(client_socket,)
+            )
+            client_thread.start()
+
+    def handle(self, client_socket):
+        if self.args.execute:
+            output = execute(self.args.execute)
+            client_socket.send(output.encode())
+
+        elif self.args.upload:
+            file_buffer = b''
+            while True:
+                data = client_socket.recv(4096)
+                if data:
+                    file_buffer += data
+                else:
+                    break
+
+            with open(self.args.upload, 'wb') as f:
+                f.write(file_buffer)
+
+            message = f'Saved file {self.args.upload}'
+            client_socket.send(message.encode())
+
+        elif self.args.commadn:
+            cmd_buffer = b''
+            while True:
+                try:
+                    client_socket.send(b'<BHP:#> ')
+                    while '\n' not in cmd_buffer.decode():
+                        cmd_buffer += client_socket.recv(64)
+                    response = execute(cmd_buffer.decode())
+
+                    if response:
+                        client_socket.send(response.encode())
+                    cmd_buffer = b''
+
+                except Exception as e:
+                    print(f'server killed {e}')
+                    self.socket.close()
+                    sys.exit()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -44,7 +134,7 @@ if __name__ == '__main__':
         )
     )
 
-    parser.add_argument('-c', '--command' action='store_true', help='command shell')
+    parser.add_argument('-c', '--command', action='store_true', help='command shell')
     parser.add_argument('-e', '--execute', help='execute specified command')
     parser.add_argument('-l', '--listen', action='store_true', help='listen')
     parser.add_argument('-p', '--port', type=int, default=5555, help='specified port')
@@ -59,18 +149,3 @@ if __name__ == '__main__':
 
     nc = NetCat(args, buffer.encode())
     nc.run()
-
-class NetCat:
-    def __init__(self, args, buffer=None):
-        self.args = args
-        self.buffer = buffer
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    def run(self):
-        if self.args.listen:
-            self.listen()
-        else:
-            self.send()
-
-    
